a47b5c2eb1706c47616abcc1ba08406a660a5152
Jgigrich <github@GreenSeaProductions.com>
2018-01-17 12:37:57 -0600

    V1 basic functionality

---
+++ script.js
@@ -0,0 +1,57 @@
+$(function(){
+  function setDefaults(){
+    if($(window).width() < 500){
+      $("#columns").attr("value", "10");
+    }
+  }
+
+  function confirmErase(grid){
+    let d = grid.find("td"), i, len;
+    for(i=0, len=d.length; i<len; i++){
+      if($(d[i]).css("background-color") !== "rgb(255, 255, 255)"){
+        return confirm("Erase current grid?");
+      }
+    }
+    return true;
+  }
+
+  function checkGrid(){
+    let grid = $(".grid");
+    if(grid.children().length === 0 || confirmErase(grid)){
+      createGrid();
+    }
+  }
+
+  function createGrid(){
+    let row = parseInt($("#rows").val(), 10),
+        col = parseInt($("#columns").val(), 10),
+       size = parseInt($("#size").val(), 10),
+       grid = $(".grid"), i, j;
+    grid.detach().empty();
+    for(i = 0; i<row; i++){
+      let tableRow = $("<tr></tr>");
+      for(j = 0; j<col; j++){
+        $("<td></td>")
+          .css({"background-color": "rgb(255, 255, 255)","width": size, "height": size})
+          .appendTo(tableRow);
+      }
+      tableRow.appendTo(grid);
+    }
+    grid.appendTo(".container");
+}
+
+  function changeColor(event){
+    let selectedColor = $("#color").val();
+    if($(this).css("background-color") == "rgb(255, 255, 255)"){
+      $(this).css("background-color", selectedColor);
+    }
+    else{
+      $(this).css("background-color", "rgb(255, 255, 255)")
+    }
+  }
+
+  $(".grid").on("click", "td", changeColor);
+  $("#createGrid").on("click", checkGrid);
+  setDefaults();
+  $("#createGrid").trigger("click");
+});
